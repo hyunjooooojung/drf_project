22a02af761eefe3db294fd471242022a03416000
hyunjooooojung <hyunjoo3107@gmail.com>
2022-10-26 22:19:16 +0900

    media 파일 업로드 위해 설정, model 설계

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+
 
 # Create your views here.
+class ArticleView(APIView):
+    def get(self, request):
+        pass
+    def post(self, request):
+        pass
+    
+class ArticleDetailView(APIView):
+    def get(self, request, article_id):
+        pass
+    def put(self, request, article_id):
+        pass
+    def delete(self, request, article_id):
+        pass
