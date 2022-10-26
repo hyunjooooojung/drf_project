46e0dcdc4ec7760bad8765008c315ade09ccd63c
hyunjooooojung <hyunjoo3107@gmail.com>
2022-10-27 01:43:39 +0900

    article & detail

--- articles/urls.py
+++ articles/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.ArticleView.as_view(), name='article_view'),
     path('<int:article_id>/', views.ArticleDetailView.as_view(), name='article_detail_view'),
-    # path('comment/', views.CommentView.as_view(), name='comment_view'),
-    # path('comment/<int:comment_id>/', views.CommentDetailView.as_view(), name='comment_detail_view'),
-    # path('like/', views.LikeView.as_view(), name='like_view'),
+    path('<int:article_id>/comment/', views.CommentView.as_view(), name='comment_view'),
+    path('<int:article_id>/comment/<int:comment_id>/', views.CommentDetailView.as_view(), name='comment_detail_view'),
+    path('<int:article_id>/like/', views.LikeView.as_view(), name='like_view'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- articles/views.py
+++ articles/views.py
@@ -1,19 +1,90 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework import status, permissions
 from rest_framework.response import Response
+from articles.models import Article, Comment
+from articles.serializers import ArticleSerializer, ArticleListSerializer, ArticleCreateSerializer, CommentSerializer, CommentCreateSerializer
 
 
 # Create your views here.
 class ArticleView(APIView):
     def get(self, request):
-        pass
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
     def post(self, request):
-        pass
+        serializer = ArticleCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     
 class ArticleDetailView(APIView):
     def get(self, request, article_id):
-        pass
+        article = get_object_or_404(Article, id=article_id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
     def put(self, request, article_id):
-        pass
+        article = get_object_or_404(Article, id=article_id)
+        if request.user == article.user:
+            serializer = ArticleCreateSerializer(article, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
+        
     def delete(self, request, article_id):
-        pass
+        article = get_object_or_404(Article, id=article_id)
+        if request.user == article.user:
+            article.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
+
+class CommentView(APIView):
+    def get(self, request, article_id):
+        article = Article.objects.get(id=article_id)
+        comments = article.comment_set.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, article_id):
+        serializer = CommentCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, article_id=article_id)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetailView(APIView):
+    def put(self, request, article_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            serializer = CommentCreateSerializer(comment, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
+ 
+    def delete(self, request, article_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
+        
+        
+class LikeView(APIView):
+    pass
