95045eebc36f5e46b6516c42ceab70b7fd1300e7
hyunjooooojung <hyunjoo3107@gmail.com>
2022-10-27 10:37:06 +0900

    게시글 상세페이지 수정, 좋아요 갯수, 댓글 갯수 추가

--- articles/models.py
+++ articles/models.py
@@ -18,7 +18,7 @@ class Article(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- articles/serializers.py
+++ articles/serializers.py
@@ -3,9 +3,16 @@ from rest_framework import serializers
 from articles.models import Article, Comment
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    
+    # user의 값을 email로 받아오는 것으로 변경
+    def get_user(self, obj):
+        return obj.user.email
+    
     class Meta:
         model = Comment
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ('article',)
     
      
 class CommentCreateSerializer(serializers.ModelSerializer):
@@ -16,7 +23,11 @@ class CommentCreateSerializer(serializers.ModelSerializer):
 
 class ArticleSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
-        
+    # comment는 Foreign key로 받아오고 있기 때문에 역참조 set 사용! or related_name 설정했기 때문에 comments로 받아옴
+    comments = CommentSerializer(many=True) 
+    # 좋아요 눌렀을 때 누른 사람 username이 나오도록 변경
+    likes = serializers.StringRelatedField(many=True)
+    
     # user의 값을 email로 받아오는 것으로 변경
     def get_user(self, obj):
         return obj.user.email
@@ -34,11 +45,20 @@ class ArticleCreateSerializer(serializers.ModelSerializer):
         
 class ArticleListSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+    
     
     # user의 값을 email로 받아오는 것으로 변경
     def get_user(self, obj):
         return obj.user.email
     
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+    
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+    
     class Meta:
         model = Article
-        fields = ('pk', 'title', 'image', 'updated_at', 'user')
+        fields = ('pk', 'title', 'image', 'updated_at', 'user', 'likes_count', 'comments_count')
