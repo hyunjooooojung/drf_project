a14851638703fdb3d74be4bece12175507defff4
hyunjooooojung <hyunjoo3107@gmail.com>
2022-10-27 10:17:17 +0900

    좋아요 기능, 팔로우 기능 추가

--- articles/views.py
+++ articles/views.py
@@ -87,4 +87,11 @@ class CommentDetailView(APIView):
         
         
 class LikeView(APIView):
-    pass
+    def post(self, request, article_id):
+        article = get_object_or_404(Article, id=article_id)
+        if request.user in article.likes.all():
+            article.likes.remove(request.user)
+            return Response("좋아요 했습니다!", status=status.HTTP_200_OK)
+        else:
+            article.likes.add(request.user)
+            return Response("좋아요 취소 했습니다!", status=status.HTTP_200_OK)
